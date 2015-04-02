513a847d06f1550e9b6c9810a5b5b28df2fd205a
Japhet Marcos <elielmarcos@gmail.com>
2015-04-03 02:23:35 +0800

    Changed database.php

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
+			'host'      => env('DB_HOST', '45.55.175.42'),
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
